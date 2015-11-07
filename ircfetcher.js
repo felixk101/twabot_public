a0cad09b8b2df93ef72ca7af9ed23c42ec5df854
Lukas Philipp Edlboeck <lukasphilipp.edlboeck@hs-augsburg.de>
2015-11-07 20:52:36 +0100

    Twitch Anbindung

--- ircfetcher.js
+++ ircfetcher.js
@@ -1,44 +1,76 @@
-var net = require('net');
-
+"use strict"
+/**
+ * Created by Lukas on 07.11.2015.
+ */
+var net=require('net');
 var HOST = 'irc.twitch.tv';
 var PORT = 6667;
 var NICK = 'hochschuleaugsburg';
 var PASS = 'oauth:lhq4ad0tqxgkxu9kxbo65jtpppqm47';
+var CHAN = '#lirik'
 
-var CHAN = 'starcraft';
+var options={port:PORT,host:HOST};
 
-var client = new net.Socket();
+function get_message(data){
+	let returnValue='';
+	if(data.length-1>1){
 
-function fetch (channel) {
-	client.connect(PORT, HOST);
-	console.log('maybe connected now?');
-	client.connect(PORT, HOST, function() {
-		console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-		client.write('PASS '+PASS+'%s\r\n');
-		client.write('NICK '+NICK+'%s\r\n');
-		//client.write('JOIN '+CHAN+'%s\r\n');
-		console.log('send stuff');
-	});
-	
-	client.on('connect', function(idk) {
-		console.log('idk:',idk);
-	});
-	client.on('data', function(data) {
-		console.log('DATA: ' + data);
-		// Close the client socket completely
-		//client.destroy();
-	});
-	
-	client.on('close', function() {
-    	console.log('Connection closed');
+		data=data.splice(1);
+		returnValue=data.join(':');
+
+
+	}else{
+		returnValue=data[1];
+	}
+	returnValue=returnValue.replace(/\r\n/g,'');
+	return returnValue;
+}
+
+function get_sender(data){
+
+	let user=data[0].split('!');
+
+	user[1]=user[1].split('@')[0];
+	return user[0];
+}
+
+function getTimestamp(){
+	let date=new Date();
+	return (date.getDay()+'.'+date.getMonth()+'.'+date.getFullYear()+'|'+date.getHours() +
+	':'+date.getMinutes()+':'+date.getSeconds()+':'+date.getMilliseconds());
+
+}
+
+function connectToChat() {
+	var client = net.connect(options, function () {
+		console.log('Connected to Server');
+		client.write('PASS ' + PASS + '\r\n');
+		client.write('NICK ' + NICK + '\r\n');
+		client.write('JOIN ' + CHAN + '\r\n');
+
+		client.on('data', function (data) {
+			data = data.toString();
+			let input = data.split(':');
+			if (input[0] === '') {
+				input = input.splice(1);
+			}
+			if (input[0].split(' ')[0] === 'PING') {
+				client.write('PONG ' + input[1] + '\r\n');
+			} else if (input[0].split(' ')[0] === 'PART') {
+
+			} else if (input[0].split(' ')[1] == 'PRIVMSG') {
+
+				let message = get_message(input);
+				let sender = get_sender(input);
+				console.log(getTimestamp()+'|'+sender + ":" + message);
+			}
+		});
+		client.on('end', function () {
+			console.log('Disconectd');
+		});
 	});
 }
 
-try {
-	fetch();
-} catch (e) {
-	console.log('caught an error:',e);
-} finally {
-	client.destroy();
-	console.log('end of program');
-}
+connectToChat();
+
+
