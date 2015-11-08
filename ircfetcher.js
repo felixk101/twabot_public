6cbd6e31ded63f7b57aac896c18c8a01325c4faf
Felix Kampfer <kampferf@rz.hs-augsburg.de>
2015-11-08 14:23:32 +0100

    get chat server from twitch's api (instead of just hoping it'll work)

--- ircfetcher.js
+++ ircfetcher.js
@@ -1,15 +1,38 @@
 "use strict"
 /**
- * Created by Lukas on 07.11.2015.
+ * Created by Lukas on 07.11.2015. modified by felix 08.11.15
  */
 var net=require('net');
-var HOST = 'irc.twitch.tv';
-var PORT = 6667;
+var request=require('request');
+var host ;//= 'irc.twitch.tv';
+var port ;//= 6667;
 var NICK = 'hochschuleaugsburg';
 var PASS = 'oauth:lhq4ad0tqxgkxu9kxbo65jtpppqm47';
-var CHAN = '#lirik'
 
-var options={port:PORT,host:HOST};
+var chan = process.argv[2] || 'bobross'; //no # sign!
+
+
+function fetchChat(chan){
+	console.log('Looking up https://api.twitch.tv/api/channels/'+chan+'/chat_properties');
+	//Ask Twitch for information about the channel, including chat servers
+	request({
+		url: 'https://api.twitch.tv/api/channels/'+chan+'/chat_properties',
+		json: true
+	}, function (error, response, body) {
+		
+		if (!error && response.statusCode === 200) {
+			//Use the first chat server for the channel that Twitch's API gives us
+			host=body.chat_servers[0].split(":")[0];
+			port=parseInt(body.chat_servers[0].split(":")[1]);
+			//callback
+			connectToChat({host: host, port: port});
+		} else {
+			console.log('ERROR: Could not find any info about channel \''+chan+'\'. (error:'+error+')');
+			return {error};
+		}
+	})
+	
+};
 
 function get_message(data){
 	let returnValue='';
@@ -17,7 +40,6 @@ function get_message(data){
 
 		data=data.splice(1);
 		returnValue=data.join(':');
-
 
 	}else{
 		returnValue=data[1];
@@ -41,12 +63,13 @@ function getTimestamp(){
 
 }
 
-function connectToChat() {
+function connectToChat(options) {
+	console.log('Connecting to server with arguments:',options);
 	var client = net.connect(options, function () {
 		console.log('Connected to Server');
 		client.write('PASS ' + PASS + '\r\n');
 		client.write('NICK ' + NICK + '\r\n');
-		client.write('JOIN ' + CHAN + '\r\n');
+		client.write('JOIN #' + chan + '\r\n');
 
 		client.on('data', function (data) {
 			data = data.toString();
@@ -70,7 +93,8 @@ function connectToChat() {
 		});
 	});
 }
-
-connectToChat();
+//var options={port:port,host:host};
+fetchChat(chan);
+//connectToChat();
 
 
