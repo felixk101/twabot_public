9f0847dac64c61c658a393095609558f380cd336
Andreas Wundlechner <andreas.wundlechner@hs-augsburg.de>
2015-12-10 21:35:47 +0100

    updated some bootstrap functionality and corresponding buildscriptsettings

--- interface/buildScript.js
+++ interface/buildScript.js
@@ -5,7 +5,7 @@
 // first npm install then run this script
 let exec = require('child_process').execSync;
 let platform = require('os').platform();
-let update = require('./updateBuildScript.js');
+let update = require('./buildUpdateScript');
 let folder = "public";
 
 function build() {
@@ -13,11 +13,17 @@ function build() {
         console.log('Try to delete existing "' + folder + '" folder.');
         try {
             exec("rmdir /S /Q " + folder);
+            console.log("Success");
         } catch (err) {}
         exec("mkdir " + folder);
 
+        // Copy the bootstrap things
         exec("mkdir " + folder + "\\node_modules\\bootstrap");
         exec("xcopy /E node_modules\\bootstrap " + folder + "\\node_modules\\bootstrap");
+
+        // Copy the jQuery things for bootstrap
+        exec("mkdir " + folder + "\\node_modules\\jquery");
+        exec("xcopy /E node_modules\\jquery " + folder + "\\node_modules\\jquery");
 
         exec("copy favicon.ico " + folder + "\\favicon.ico");
         exec("copy about.html " + folder + "\\about.html");
@@ -26,12 +32,17 @@ function build() {
         console.log('Try to delete existing "' + folder + '" folder.');
         try {
             exec("rm -r " + folder);
+            console.log("Success");
         } catch (err) {}
         exec("mkdir " + folder);
 
         exec("mkdir " + folder + "/node_modules");
         exec("mkdir " + folder + "/node_modules/bootstrap");
         exec("cp -r node_modules/bootstrap " + folder + "/node_modules/bootstrap");
+
+        // Copy the jQuery things for bootstrap
+        exec("mkdir " + folder + "/node_modules/jquery");
+        exec("cp -r node_modules/jquery " + folder + "/node_modules/jquery");
 
         exec("cp favicon.ico " + folder + "/favicon.ico");
         exec("cp about.html " + folder + "/about.html");
