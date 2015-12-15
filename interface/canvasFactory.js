842c5f5c27833e916393bda5056ab3fa2c3432b7
Andreas Wundlechner <andreas.wundlechner@hs-augsburg.de>
2015-12-15 15:28:04 +0100

    Refactored the thumbnail creation

--- interface/canvasFactory.js
+++ interface/canvasFactory.js
@@ -1,114 +1,70 @@
 "use strict";
 
 let Chart = require('chart.js');
+let TransformOptions = require('./TransformOptions');
 
 exports.createThumbnail = function createThumbnail(canvas, channel){
     let ctx = canvas.getContext("2d");
 
-    let transformOptionsPrototyp = {
-        scaleX: 1,
-        scaleY: 1,
-        translateX: 0,
-        translateY: 0,
-        width: canvas.width,
-        height: canvas.height,
-        setScale: function(x,y){
-            this.scaleX = x;
-            this.scaleY = y;
-        },
-        /**
-         * Translation is relative to the width and heigth.
-         * Example: Width is 300, x is 2/3 => the drawing starts at 200
-         * @param x
-         * @param y
-         */
-        setTranslation: function(x,y){
-            this.translateX = x;
-            this.translateY = y;
-        },
-        apply: function(ctx){
-            ctx.scale(this.scaleX, this.scaleY);
-            ctx.translate(this.translateX * this.width / this.scaleX,
-                this.translateY * this.height / this.scaleY);
-        }
-    };
+    // Old positions
+    let positions = [
+        new TransformOptions(canvas.width, canvas.height, 2/3, 2/3),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 2/3, 0),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 2/3, 1/3),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 0, 2/3),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 1/3, 2/3),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 2/3, 2/3)
+    ];
 
-    // Logo
-    ctx.save();
-    let options = Object.create(transformOptionsPrototyp);
-    options.setScale(2/3, 2/3);
-    drawChannelLogo(channel.logo, ctx, options);
-    ctx.restore();
+    positions = [
+        new TransformOptions(canvas.width, canvas.height, 2/3, 1),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 2/3, 0),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 2/3, 1/3),
+        new TransformOptions(canvas.width, canvas.height, 1/3, 1/3, 2/3, 2/3)
+    ];
 
-    // Fractal
-    ctx.save();
-    options = Object.create(transformOptionsPrototyp);
-    options.setScale(1/3, 1/3); // The fractal is 1/3 of the complete canvas size
-    options.setTranslation(2/3, 0); // Tho position relative to the size
-    drawFractal(channel.name, ctx, options);
-    ctx.restore();
+    let drawFunctions = [
+        drawChannelLogo,
+        drawFractal,
+        drawDiagram1,
+        drawDiagram2//,
+        //drawDiagram3,
+        //drawDiagram4
+    ];
 
-    // Diagram1 // for calculation look at //Fractal
-    ctx.save();
-    options = Object.create(transformOptionsPrototyp);
-    options.setScale(1/3, 1/3);
-    options.setTranslation(2/3, 1/3);
-    drawDiagram1(channel.name, ctx, options);
-    ctx.restore();
+    for (let i=0; i<positions.length; i++){
+        drawFunctions[i](channel, ctx, positions[i]);
+    }
 
-    // Diagram2 // for calculation look at //Fractal
-    ctx.save();
-    options = Object.create(transformOptionsPrototyp);
-    options.setScale(1/3, 1/3);
-    options.setTranslation(0, 2/3);
-    drawDiagram2(channel.name, ctx, options);
-    ctx.restore();
-
-    // Diagram3 // for calculation look at //Fractal
-    ctx.save();
-    options = Object.create(transformOptionsPrototyp);
-    options.setScale(1/3, 1/3);
-    options.setTranslation(1/3, 2/3);
-    drawDiagram3(channel.name, ctx, options);
-    ctx.restore();
-
-    // Diagram4 // for calculation look at //Fractal
-    ctx.save();
-    options = Object.create(transformOptionsPrototyp);
-    options.setScale(1/3,1/3);
-    options.setTranslation(2/3, 2/3);
-    drawDiagram4(channel.name, ctx, options);
-    ctx.restore();
 };
 
 
-function drawChannelLogo(channelLogo, ctx, options){
+function drawChannelLogo(channel, ctx, options){
     let img = new Image();
     img.addEventListener("load", function(){
         ctx.save();
-        options.apply(ctx);// text
-        console.log("hi");// text
+        options.apply(ctx);
         ctx.drawImage(img, 0, 0, options.width, options.height);
         ctx.restore();
     },false);
-    img.src = channelLogo;
+    img.src = channel.logo;
 }
 
-function drawFractal(channelName, ctx, options){
+function drawFractal(channel, ctx, options){
     // Dummy
     ctx.save();
     options.apply(ctx);
     ctx.fillStyle = "red";
     ctx.fillRect(0,0,options.width, options.height);
     ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillText(channelName,10,10);
+    ctx.fillText(channel.name,10,10);
     ctx.restore();
 }
 
-function drawDiagram1(channelName, ctx, options){
+function drawDiagram1(channel, ctx, options){
     ctx.save();
     options.apply(ctx);
-    let data = getEmotions(channelName);
+    let data = getEmotions(channel.name);
     
     // Building a new Canvas for the Diagramm
     let diagramCanvas = document.createElement("canvas");
@@ -139,10 +95,10 @@ function drawDiagram1(channelName, ctx, options){
     ctx.restore();
 }
 
-function drawDiagram2(channelName, ctx, options){
+function drawDiagram2(channel, ctx, options){
     ctx.save();
     options.apply(ctx);
-    let data = getEmotions(channelName);
+    let data = getEmotions(channel.name);
 
     // Building a new Canvas for the Diagramm
     let diagramCanvas = document.createElement("canvas");
@@ -167,25 +123,25 @@ function drawDiagram2(channelName, ctx, options){
     ctx.restore();
 }
 
-function drawDiagram3(channelName, ctx, options){
+function drawDiagram3(channel, ctx, options){
     // Dummy
     ctx.save();
     options.apply(ctx);
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, options.width, options.height);
     ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillText(channelName,10,10);
+    ctx.fillText(channel.name,10,10);
     ctx.restore();
 }
 
-function drawDiagram4(channelName, ctx, options){
+function drawDiagram4(channel, ctx, options){
     // Dummy
     ctx.save();
     options.apply(ctx);
     ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, options.width, options.height);
     ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillText(channelName,10,10);
+    ctx.fillText(channel.name,10,10);
     ctx.restore();
 }
 
